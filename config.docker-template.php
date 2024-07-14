09d596e489bceb8c6b53b4b4f475809da2f3887d
eticialima <leticialimacgi@gmail.com>
2024-07-14 20:32:14 -0300

    Cria docker compose com imagem moodlehq

---
+++ config.docker-template.php
@@ -0,0 +1,44 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('MOODLE_DOCKER_DBTYPE');
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'moodledb';
+$CFG->dbname    = getenv('MOODLE_DOCKER_DBNAME');
+$CFG->dbuser    = getenv('MOODLE_DOCKER_DBUSER');
+$CFG->dbpass    = getenv('MOODLE_DOCKER_DBPASS');
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$host = 'localhost';
+if (!empty(getenv('MOODLE_DOCKER_WEB_HOST'))) {
+    $host = getenv('MOODLE_DOCKER_WEB_HOST');
+}
+$CFG->wwwroot   = "http://{$host}";
+$port = getenv('MOODLE_DOCKER_WEB_PORT'); // 8080
+if (!empty($port)) {
+    // Extract port in case the format is bind_ip:port.
+    $parts = explode(':', $port);
+    $port = end($parts);
+    if ((string)(int)$port === (string)$port) { // Only if it's int value.
+        $CFG->wwwroot .= ":{$port}";
+    }
+}
+
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
